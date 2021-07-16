6bbf219cd6c9fdde80c74b198724a7fa540b2f9a
Azeem119 <63467203+Azeem119@users.noreply.github.com>
2021-07-16 14:45:29 +0530

    Add files via upload

---
+++ Project/todosite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from todo import views
+from rest_framework import viewsets, routers
+
+urlpatterns = patterns('',
+    # Registration of new users
+    url(r'^register/$', views.RegistrationView.as_view()),
+
+    # Todos endpoint
+    url(r'^todos/$', views.TodosView.as_view()),
+    url(r'^todos/(?P<todo_id>[0-9]*)$', views.TodosView.as_view()),
+
+    # API authentication
+    url(r'^oauth2/', include('provider.oauth2.urls', namespace='oauth2')),
+    url(r'^api-auth/', include('rest_framework.urls',\
+        namespace='rest_framework')),
+)
+
